8c2379c405f37dc976d3894566a491c10b7b887b
Ahmad Mahdaly <ahmed.mahdaly86@gmail.com>
2025-04-08 23:54:05 +0200

    Add color constants for consistent theming across the app

---
+++ lib/utils/constants/colors_constants.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const kMainColor = Color(0xFF2A5E99);
+const kScaffoldBackgroundColor = Colors.white;
+const kTextColor = Color(0xFF686868);
+const kLightBlueColor = Color(0xff35B3F1);
+const kOrangeColor = Color(0xffF15513);
+const kBigHeaderTitleColor = Color(0xFF4D4D4D);
