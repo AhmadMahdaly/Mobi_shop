2319027f746b76d15b3f2db6c4fee4111d978b7a
Ahmad Mahdaly <ahmed.mahdaly86@gmail.com>
2025-04-09 17:20:36 +0200

    Add CustomHeadTitle widget for displaying a title with an action button

---
+++ lib/screens/home_page/widgets/custom_head_title.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:mobi_shop/utils/constants/colors_constants.dart';
+
+class CustomHeadTitle extends StatelessWidget {
+  const CustomHeadTitle({required this.title, this.onPressed, super.key});
+  final void Function()? onPressed;
+  final String title;
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 16.w),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            title,
+            style: TextStyle(
+              color: kBigHeaderTitleColor,
+              fontSize: 20.sp,
+              fontWeight: FontWeight.w700,
+              height: 1.56.h,
+            ),
+          ),
+          IconButton(onPressed: () {}, icon: const Icon(Icons.arrow_forward)),
+        ],
+      ),
+    );
+  }
+}
