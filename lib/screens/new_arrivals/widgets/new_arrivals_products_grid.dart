2419c4901a659526fdf04c503593eb32fbbb4f8e
Ahmad Mahdaly <ahmed.mahdaly86@gmail.com>
2025-04-10 01:20:16 +0200

    Add NewArrivalsScreen and NewArrivalsProductsGrid for displaying new arrival products

---
+++ lib/screens/new_arrivals/widgets/new_arrivals_products_grid.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:mobi_shop/widgets/item_widget/item_widget.dart';
+
+class NewArrivalsProductsGrid extends StatelessWidget {
+  const NewArrivalsProductsGrid({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverGrid(
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        childAspectRatio: 0.65.sp,
+        mainAxisSpacing: 9.h,
+        crossAxisSpacing: 9.w,
+      ),
+      delegate: SliverChildBuilderDelegate(
+        (context, index) => const ItemWidget(),
+        childCount: 19,
+        semanticIndexOffset: 16,
+      ),
+    );
+  }
+}
