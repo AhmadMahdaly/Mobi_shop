d6f26f985b846f864feae857e6a85edc302e0b9a
Ahmad Mahdaly <ahmed.mahdaly86@gmail.com>
2025-04-09 20:30:51 +0200

    Add NewArrivalsWidget for displaying new arrival items in a grid layout

---
+++ lib/screens/home_page/widgets/new_arrivals_widget.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:mobi_shop/widgets/item_widget/item_widget.dart';
+
+class NewArrivalsWidget extends StatelessWidget {
+  const NewArrivalsWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 350.h,
+      child: GridView.builder(
+        padding: EdgeInsets.symmetric(horizontal: 16.w),
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 0.6.sp,
+          mainAxisSpacing: 9.h,
+          crossAxisSpacing: 18.w,
+        ),
+        itemBuilder: (context, index) => const ItemWidget(),
+        itemCount: 2,
+      ),
+    );
+  }
+}
