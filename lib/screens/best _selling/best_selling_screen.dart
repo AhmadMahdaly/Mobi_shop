6ab07a70e06817c39408d4ecab78bddb55d1c612
Ahmad Mahdaly <ahmed.mahdaly86@gmail.com>
2025-04-10 01:20:31 +0200

    Add BestSellingScreen and BestSellingProductsGrid for displaying best-selling products

---
+++ lib/screens/best _selling/best_selling_screen.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:mobi_shop/screens/best%20_selling/widgets/best_selling_products_grid.dart';
+import 'package:mobi_shop/screens/home_page/widgets/custom_search_app_bar.dart';
+import 'package:mobi_shop/widgets/custom_app_bar_pages.dart';
+import 'package:mobi_shop/widgets/filter_button/filter_button.dart';
+import 'package:mobi_shop/widgets/page_title_widget.dart';
+import 'package:mobi_shop/widgets/qty_results_widget.dart';
+import 'package:mobi_shop/widgets/sort_button/sort_button.dart';
+
+class BestSellingScreen extends StatelessWidget {
+  const BestSellingScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16.w),
+            child: const CustomScrollView(
+              slivers: [
+                SliverAppBar(
+                  flexibleSpace: CustomAppBarPages(),
+                  automaticallyImplyLeading: false,
+                ),
+                SliverToBoxAdapter(child: CustomSearchAppBar()),
+                SliverToBoxAdapter(child: PageTitle(title: 'Best selling')),
+                SliverToBoxAdapter(child: QtyResults(44)),
+                BestSellingProductsGrid(),
+              ],
+            ),
+          ),
+          Positioned(
+            bottom: 32.h, // تحريك العنصر للخروج من الإطار
+            left: MediaQuery.of(context).size.width / 2 - 125, // منتصف الشاشة
+            child: const Row(children: [FilterButton(), SortButton()]),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/screens/best _selling/widgets/best_selling_products_grid.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:mobi_shop/widgets/item_widget/item_widget.dart';
+
+class BestSellingProductsGrid extends StatelessWidget {
+  const BestSellingProductsGrid({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverGrid(
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        childAspectRatio: 0.65.sp,
+        mainAxisSpacing: 9.h,
+        crossAxisSpacing: 9.w,
+      ),
+      delegate: SliverChildBuilderDelegate(
+        (context, index) => const ItemWidget(),
+        childCount: 19,
+        semanticIndexOffset: 16,
+      ),
+    );
+  }
+}
