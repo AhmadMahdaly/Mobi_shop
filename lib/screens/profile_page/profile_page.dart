f168876049dd70bb567bc48492d97da15edcbaed
Ahmad Mahdaly <ahmed.mahdaly86@gmail.com>
2025-04-11 00:51:31 +0200

    Add ProfilePage implementation with login support

---
+++ lib/screens/profile_page/profile_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:mobi_shop/screens/profile_page/widgets/profile_page_with_login.dart';
+
+class ProfilePage extends StatelessWidget {
+  const ProfilePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const ProfileBodyWithLogin();
+  }
+}
