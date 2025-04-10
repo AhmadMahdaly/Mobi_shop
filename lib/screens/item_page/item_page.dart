2a5737ad307caa64d4d607ed9b198f28436dc84b
Ahmad Mahdaly <ahmed.mahdaly86@gmail.com>
2025-04-10 03:18:18 +0200

    Add ItemPage and integrate navigation from ItemWidget

---
+++ lib/screens/item_page/item_page.dart
@@ -0,0 +1,113 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:mobi_shop/utils/components/custom_button.dart';
+import 'package:mobi_shop/utils/components/space.dart';
+import 'package:mobi_shop/utils/constants/colors_constants.dart';
+
+class ItemPage extends StatelessWidget {
+  const ItemPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(automaticallyImplyLeading: false),
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 16.w),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Align(
+              alignment: Alignment.topCenter,
+              child: Image.asset(
+                'assets/images/product 2.jpg',
+                width: 330.w,
+                height: 329.h,
+              ),
+            ),
+            const H(h: 16),
+            SizedBox(
+              width: 330.w,
+              child: Text(
+                'Redmi Xiaomi 12 8GB RAM, 128GB ',
+                style: TextStyle(
+                  color: kMainColor,
+                  fontSize: 16.sp,
+                  fontWeight: FontWeight.w600,
+                  height: 1.h,
+                ),
+              ),
+            ),
+            const H(h: 16),
+
+            Row(
+              children: [
+                Text(
+                  '6700',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 20.sp,
+                    fontWeight: FontWeight.w700,
+                    height: 1.40.h,
+                  ),
+                ),
+                W(w: 2.w),
+                Column(
+                  children: [
+                    Text(
+                      '.00',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 10.sp,
+                        fontWeight: FontWeight.w700,
+                        height: 1.h,
+                      ),
+                    ),
+                    Text(
+                      'EGP',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 7.sp,
+                        fontWeight: FontWeight.w400,
+                        height: 1.h,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            const H(h: 10),
+            const Divider(color: kBorderColor, thickness: 1.2),
+            const H(h: 10),
+            Text(
+              'About this Mobile ',
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 14.sp,
+                fontWeight: FontWeight.w600,
+                height: 1.14.h,
+              ),
+            ),
+            const H(h: 10),
+
+            SizedBox(
+              width: 350,
+              child: Text(
+                'Redmi Xiaomi 12 8GB RAM, 128GB ROM 90Hz 6.79” FHD+ display Large 5000mAh (typ) battery 50MP Al triple camera IP53 dust and splash resistant Large 90Hz FHD 1 year local warranty, Midnight Black',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 13.sp,
+                  fontWeight: FontWeight.w400,
+                  height: 1.23.h,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: Padding(
+        padding: EdgeInsets.all(16.w),
+        child: const CustomButton(text: 'Add to Cart'),
+      ),
+    );
+  }
+}
--- lib/widgets/item_widget/item_widget.dart
+++ lib/widgets/item_widget/item_widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:mobi_shop/models/item_model.dart';
+import 'package:mobi_shop/screens/item_page/item_page.dart';
 import 'package:mobi_shop/utils/components/space.dart';
 import 'package:mobi_shop/utils/constants/colors_constants.dart';
 import 'package:mobi_shop/widgets/item_widget/widgets/add_button_product_to_cart.dart';
@@ -21,39 +22,46 @@ class ItemWidget extends StatelessWidget {
       image: 'assets/images/product 1.png',
       comment: 'Only 1 left!',
     );
-    return Container(
-      width: 185.w,
-      height: 330.h,
-      clipBehavior: Clip.antiAlias,
-      decoration: ShapeDecoration(
-        color: kScaffoldBackgroundColor,
-        shape: RoundedRectangleBorder(
-          side: BorderSide(width: 1.w, color: kBorderColor),
-          borderRadius: BorderRadius.circular(16.r),
+    return InkWell(
+      onTap:
+          () => Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => const ItemPage()),
+          ),
+      child: Container(
+        width: 185.w,
+        height: 330.h,
+        clipBehavior: Clip.antiAlias,
+        decoration: ShapeDecoration(
+          color: kScaffoldBackgroundColor,
+          shape: RoundedRectangleBorder(
+            side: BorderSide(width: 1.w, color: kBorderColor),
+            borderRadius: BorderRadius.circular(16.r),
+          ),
         ),
-      ),
-      child: Stack(
-        children: [
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const BadgeSpecialOffer(),
-              const H(h: 5),
-              ProductImage(item: item),
-              const H(h: 5),
-              ProductPrice(item: item),
-              const H(h: 10),
-              ProductTitle(item: item),
-              const H(h: 10),
-              ItemComment(item: item),
-            ],
-          ),
-          Positioned(
-            right: -5.w,
-            bottom: 90.h,
-            child: const AddButtonProductToCart(),
-          ),
-        ],
+        child: Stack(
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const BadgeSpecialOffer(),
+                const H(h: 5),
+                ProductImage(item: item),
+                const H(h: 5),
+                ProductPrice(item: item),
+                const H(h: 10),
+                ProductTitle(item: item),
+                const H(h: 10),
+                ItemComment(item: item),
+              ],
+            ),
+            Positioned(
+              right: -5.w,
+              bottom: 90.h,
+              child: const AddButtonProductToCart(),
+            ),
+          ],
+        ),
       ),
     );
   }
